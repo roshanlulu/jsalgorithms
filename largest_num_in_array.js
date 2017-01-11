e23ce4259646cc4b75aba4ec9516558e46755a72
Roshan Lulu <roshanlulu@gmail.com>
2017-01-11 23:04:40 +1100

    Refactoring using es6 sytax

--- largest_num_in_array.js
+++ largest_num_in_array.js
@@ -1,15 +1,12 @@
+const descending = (a, b) => (b - a);
 
-function sortsubarray(a, b){
-  return b - a;
+const getLargestNum = (subArray) => {
+  subArray.sort(descending);
+  return subArray[0];
 }
 
-function getLargestNum(subarray) {
-  subarray.sort(sortsubarray);
-  return subarray[0];
-}
+const largestOfFour = arr => arr.map(getLargestNum);
 
-function largestOfFour(arr) {
-  return arr.map(getLargestNum);
-}
+const arr = [[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]];
 
-console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
+console.log(" New array = " + largestOfFour(arr));
